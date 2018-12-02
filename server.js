a84a99f4b2578d36b5771351ed2ab301ec562fea
Henry Chen <chenh@students.hale.wa.edu.au>
2018-12-02 13:25:49 +0800

    set up express basics w/ mongoose connectivity

--- server.js
+++ server.js
@@ -3,5 +3,13 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 const app = express()
+const db = 'mongodb://localhost:27017/store'
 
 app.use(bodyParser.json())
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(console.log(`connected to mongodb database...`))
+    .catch(err => console.log(err))
+
+app.listen(3000,() => {console.log(`Server Started`)})
